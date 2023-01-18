4be40b262e71e140682a8495106e753019f0ed0c
gargashi <ashishgarg2709@gmail.com>
2023-01-18 11:37:07 +0530

    all chnages

--- index.js
+++ index.js
@@ -11,64 +11,99 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 
+// By default display none
+document.getElementById("page-1").style.display = "none";
+document.getElementById("page-2").style.display = "none";
+document.getElementById("page-3").style.display = "none";
+document.getElementById("page-4").style.display = "none";
+document.getElementById("page-5").style.display = "none";
+
 const timeout = setTimeout(function () {
   window.location.href = "index.html#page-1";
 
   document.getElementById("homePage").classList.add("hidden");
-
-  document.getElementById("page-1").classList.remove("hidden");
-  document.getElementById("page-2").style.display = "none";
+  document.getElementById("page-1").style.display = "flex";
+  // document.getElementById("page-1").classList.remove("hidden");
+  // document.getElementById("page-2").style.display = "none";
 
   window.clearTimeout(timeout);
-}, 5000);
+}, 2000);
 
 function secondId() {
   window.location.href = "index.html#page-2";
   document.getElementById("page-2").style.display = "flex";
+  // document.getElementById("page-2").classList.remove("hidden");
+  // document.getElementById("page-1").classList.add("hidden");
+  document.getElementById("page-1").style.display = "none";
 
-  document.getElementById("page-2").classList.remove("hidden");
-  document.getElementById("page-1").classList.add("hidden");
 }
 function thirdId() {
   window.location.href = "index.html#page-3";
-  document.getElementById("page-3").classList.remove("hidden");
-  document.getElementById("page-2").classList.add("hidden");
+  // document.getElementById("page-3").classList.remove("hidden");
+  // document.getElementById("page-2").classList.add("hidden");
+
+  document.getElementById("page-3").style.display = "flex";
+  document.getElementById("page-2").style.display = "none";
 }
 function fourthId() {
   window.location.href = "index.html#page-4";
-  document.getElementById("page-4").classList.remove("hidden");
-  document.getElementById("page-3").classList.add("hidden");
-  document.getElementById("page-2").classList.add("hidden");
-  document.getElementById("page-5").classList.add("hidden");
+  // document.getElementById("page-4").classList.remove("hidden");
+  document.getElementById("page-4").style.display = "flex";
+  // document.getElementById("page-3").classList.add("hidden");
+  document.getElementById("page-3").style.display = "none";
+
+  // document.getElementById("page-2").classList.add("hidden");
+  document.getElementById("page-2").style.display = "none";
+
+  // document.getElementById("page-5").classList.add("hidden");
+  document.getElementById("page-5").style.display = "none";
+
 }
 function fifthId() {
   window.location.href = "index.html#page-5";
-  document.getElementById("page-5").classList.remove("hidden");
-  document.getElementById("page-4").classList.add("hidden");
-  document.getElementById("page-3").classList.add("hidden");
+  // document.getElementById("page-5").classList.remove("hidden");
+  // document.getElementById("page-4").classList.add("hidden");
+  // document.getElementById("page-3").classList.add("hidden");
+
+  document.getElementById("page-5").style.display = "flex";
+  document.getElementById("page-4").style.display = "none";
+  document.getElementById("page-3").style.display = "none";
+
+
   changeModel("ANIMATION1");
 }
 function backToPage2() {
   window.location.href = "index.html#page-1";
-  document.getElementById("page-1").classList.remove("hidden");
-  document.getElementById("page-2").classList.add("hidden");
+  // document.getElementById("page-1").classList.remove("hidden");
+  document.getElementById("page-1").style.display = "flex";
+  // document.getElementById("page-2").classList.add("hidden");
   document.getElementById("page-2").style.display = "none";
 }
-function backToPage4() {
-  window.location.href = "index.html#page-2";
-  document.getElementById("page-2").classList.remove("hidden");
-  document.getElementById("page-3").classList.add("hidden");
-}
+// function backToPage4() {
+//   window.location.href = "index.html#page-2";
+//   document.getElementById("page-2").classList.remove("hidden");
+//   document.getElementById("page-3").classList.add("hidden");
+
+// }
 function backToPage3() {
   window.location.href = "index.html#page-2";
-  document.getElementById("page-2").classList.remove("hidden");
-  document.getElementById("page-4").classList.add("hidden");
-  document.getElementById("page-3").classList.add("hidden");
+  // document.getElementById("page-2").classList.remove("hidden");
+  // document.getElementById("page-4").classList.add("hidden");
+  // document.getElementById("page-3").classList.add("hidden");
+
+  document.getElementById("page-2").style.display = "flex";
+  document.getElementById("page-3").style.display = "none";
+  document.getElementById("page-4").style.display = "none";
+
 }
 function backToPage5() {
   window.location.href = "index.html#page-2";
-  document.getElementById("page-2").classList.remove("hidden");
-  document.getElementById("page-5").classList.add("hidden");
+  // document.getElementById("page-2").classList.remove("hidden");
+  // document.getElementById("page-5").classList.add("hidden");
+
+  document.getElementById("page-2").style.display = "flex";
+  document.getElementById("page-5").style.display = "none";
+
 }
 
 function changeModel(name) {
