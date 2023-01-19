ec626bef3f6531ee8c07eba6a628b458320815cd
gargashi <ashishgarg2709@gmail.com>
2023-01-19 11:22:15 +0530

    adding window resizing in js file

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const models = [
   { model: "", animation: "" },
 ];
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+// We listen to the resize event
+window.addEventListener('resize', () => {
+ // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty("--vh", `${vh}px`);
-
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 document.getElementById("page-1").style.display = "none";
 document.getElementById("page-2").style.display = "none";
